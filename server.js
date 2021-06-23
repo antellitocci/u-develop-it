8932da061a07c08922299fa4831b9e8af09c0c81
raddymaster <antellitocci@gmail.com>
2021-06-23 00:43:14 -0400

    sql election database connected

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //SQL username
+        user: 'root',
+        password: 'Ant19Chum94!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
